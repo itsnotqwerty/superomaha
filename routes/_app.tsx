be16d334ef9e2c292bdfda25a254cbafff91ecb9
Samuel Roux <samroux808@gmail.com>
2026-09-09 00:48:06 -0500

    Added embed tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,7 +12,22 @@ export default define.page(function App({ Component }) {
         />
         <meta name="theme-color" content="#0b3d2e" />
         <link rel="stylesheet" href="/styles.css" />
+
         <title>Super Omaha</title>
+        <meta name="title" content="Super Omaha" />
+        <meta name="description" content="A roguelike Omaha video poker game with procedurally generated challenges. Play through a series of increasingly difficult hands and try to reach the top of the leaderboard." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://omaha.coolfreakingames.dev/" />
+        <meta property="og:title" content="Super Omaha" />
+        <meta property="og:description" content="A roguelike Omaha video poker game with procedurally generated challenges. Play through a series of increasingly difficult hands and try to reach the top of the leaderboard." />
+        <meta property="og:image" content="https://omaha.coolfreakingames.dev/embed.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://omaha.coolfreakingames.dev/" />
+        <meta property="twitter:title" content="Super Omaha" />
+        <meta property="twitter:description" content="A roguelike Omaha video poker game with procedurally generated challenges. Play through a series of increasingly difficult hands and try to reach the top of the leaderboard." />
+        <meta property="twitter:image" content="https://omaha.coolfreakingames.dev/embed.png" />
       </head>
       <body>
         <Component />
