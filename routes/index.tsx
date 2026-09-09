0974636a2800292df983af4956e705d381368c60
Samuel Roux <samroux808@gmail.com>
2026-09-09 00:34:40 -0500

    Added donation footer

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,6 +10,10 @@ export default define.page(function Home() {
         <strong>3 board cards</strong>. Tap cards to mark them for redraw.
       </p>
       <OmahaTable />
+      <footer>
+        <p>&copy; 2026 Samuel Roux</p>
+        <p class="donate-line">Cool Freakin' Games is funded entirely by donations <a class="donate" href="bitcoin:bc1qsxmj8euqjqqze36kweglg4kut30f95gygmhyz3">&#8383; Donate Bitcoin</a></p>
+      </footer>
     </main>
   );
 });
