83cea398c08e104a51bb2f08b4a96eab65f34525
Samuel Roux <samroux808@gmail.com>
2026-09-09 00:40:21 -0500

    Added donation footer

---
+++ islands/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return(
+      <footer>
+        <p>&copy; 2026 Samuel Roux</p>
+        <p class="donate-line">Cool Freakin' Games is funded entirely by donations <a class="donate" href="bitcoin:bc1qsxmj8euqjqqze36kweglg4kut30f95gygmhyz3">&#8383; Donate Bitcoin</a></p>
+      </footer>
+    )
+}
